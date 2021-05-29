dd825211925eafa306957a5c859b774143fcb87a
Miro <miro.nexus5x@gmail.com>
2021-05-29 22:55:31 +0200

    Basic project setup; updated .gitignore and AndroidManifest, updated MainActivity with BottomNavigation and three fragments and updated some design elements (colors and some icons)

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import com.example.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val searchFragment = SearchFragment()
+        val favoritesFragment = FavoritesFragment()
+        val settingsFragment = SettingsFragment()
+
+        setFragment(searchFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.search-> {
+                    setFragment(searchFragment)
+                    binding.bottomNav.menu[1].setIcon(R.drawable.ic_star_0)}
+                R.id.favorites -> {setFragment(favoritesFragment)
+                    it.setIcon(R.drawable.ic_star_1)
+                }
+                R.id.settings -> {setFragment(settingsFragment)
+                    binding.bottomNav.menu[1].setIcon(R.drawable.ic_star_0)}
+            }
+            true
+        }
+
     }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frag_container, fragment)
+            commit()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/pokedex/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pokedex
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pokedex.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+}
